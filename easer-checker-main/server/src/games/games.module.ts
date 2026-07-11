1b9786748623a295dc462b92aeb75658d4afd1aa
KolawoleSegman <your-email@example.com>
2026-07-11 15:16:43 +0100

    Remove TypeORM imports, use Drizzle only

--- easer-checker-main/server/src/games/games.module.ts
+++ easer-checker-main/server/src/games/games.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { GamesService } from './games.service';
-import { GamesController } from './games.controller';
+import { GamesController } './games.controller';
 import { AiModule } from '../ai/ai.module';
-import { UsersModule } from '../users/users.module';
-import { FriendsModule } from '../friends/friends.module';
-import { Game } from './entities/game.entity';
-import { Move } from './entities/move.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Game, Move]), // <-- needed for repositories
-    AiModule, // if AI service is used
-    UsersModule, // if GamesService uses UsersService
-    FriendsModule, // if GamesService uses FriendsService
-  ],
+  imports: [AiModule],
   providers: [GamesService],
   controllers: [GamesController],
   exports: [GamesService],
